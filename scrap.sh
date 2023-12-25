913444f4586f67c8b5fd561cf8fdf89f55ad2ab0
_N3m0 <n3m0o7@hotmail.com>
2023-12-25 20:14:48 +0100

    rename script + get en + fr words

--- scrap.sh
+++ scrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+fr_words=$(cat /usr/share/dict/french)
+en_words=$(cat /usr/share/dict/american-english)
+
 sed -n -e "/.*defbox.*/,/^ *<\/div>$/ p" personne
 
 sed -n -e "/.*defbox.*/,/^ *<\/div>$/ p" herbe
