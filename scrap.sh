59ff1e34f3f7657434467dafcd9f1dd66b60bfd4
_N3m0 <n3m0o7@hotmail.com>
2023-12-26 14:30:38 +0100

    scrapp all word

--- scrap.sh
+++ scrap.sh
@@ -6,7 +6,8 @@ defurl="https://www.le-dictionnaire.com/resultats.php?mot="
 synurl="https://www.synonymes.com/resultats.php?mot="
 
 fr_words=$(cat /usr/share/dict/french)
-en_words=$(cat /usr/share/dict/american-english)
+#TODO
+#en_words=$(cat /usr/share/dict/american-english)
 
 dicodir="/usr/local/dico"
 langdir="fr"
@@ -17,41 +18,42 @@ dwldir="./dwl"
 sudo mkdir -p $dwldir
 sudo chown $USER:$USER -R $dwldir
 
-#word="cuisine"
-word="abaisseur"
+for word in $fr_words; do
 
-wget $defurl$word -O $dwldir/$word
-wget $synurl$word -O $dwldir/$word.syn
+  wget $defurl$word -O $dwldir/$word
+  wget $synurl$word -O $dwldir/$word.syn
 
-echo ".TH $word 1 $word\-1.0" > $dicodir/$langdir/$word
+  echo ".TH $word 1 $word\-1.0" > $dicodir/$langdir/$word
 
-sed -n -e "/.*defbox.*/,/^ *<\/div>$/ p" $dwldir/$word \
-  | sed "s/.*extraboxinfo.*/.SH/g"                     \
-  | sed "s/.*defbox.*/.SH/g"                           \
-  | sed "s/.*motboxinfo\">/.I\n/g"                     \
-  | sed "s/.*<li>/.P\n- /g"                            \
-  | sed "s/<[^>]*>//g"                                 \
-  | sed "s/^ *//g"                                     \
-  | sed "/^$/d"                                        >> $dicodir/$langdir/$word
+  sed -n -e "/.*defbox.*/,/^ *<\/div>$/ p" $dwldir/$word \
+    | sed "s/.*extraboxinfo.*/.SH/g"                     \
+    | sed "s/.*defbox.*/.SH/g"                           \
+    | sed "s/.*motboxinfo\">/.I\n/g"                     \
+    | sed "s/.*<li>/.P\n- /g"                            \
+    | sed "s/<[^>]*>//g"                                 \
+    | sed "s/^ *//g"                                     \
+    | sed "/^$/d"                                        >> $dicodir/$langdir/$word
 
-# sed cmd in okrder:
+  # sed cmd in okrder:
 
-# get only defbox divs
-# extra box info title
-# definition title
-# definition info: genre, pluriel...
-# def liste
-# rm html balise
-# rm wild indentation
-# rm empty line
+  # get only defbox divs
+  # extra box info title
+  # definition title
+  # definition info: genre, pluriel...
+  # def liste
+  # rm html balise
+  # rm wild indentation
+  # rm empty line
 
-echo -e ".SH\nSynonymes" >> $dicodir/$langdir/$word
+  echo -e ".SH\nSynonymes" >> $dicodir/$langdir/$word
 
-sed -n -e "/.*defbox.*/,/^ *<\/div>$/ p" $dwldir/$word.syn \
-  | sed "s/^ *<li>/.P\n/g"                                   \
-  | sed "s/<li>/, /g"                                      \
-  | sed "s/<[^>]*>//g"                                     \
-  | sed "s/^ *//g"                                         \
-  | sed "/^$/d"                                            >> $dicodir/$langdir/$word
+  sed -n -e "/.*defbox.*/,/^ *<\/div>$/ p" $dwldir/$word.syn \
+    | sed "s/^ *<li>/.P\n/g"                                   \
+    | sed "s/<li>/, /g"                                      \
+    | sed "s/<[^>]*>//g"                                     \
+    | sed "s/^ *//g"                                         \
+    | sed "/^$/d"                                            >> $dicodir/$langdir/$word
 
-rm $dwldir/$word $dwldir/$word.syn
+  rm $dwldir/$word $dwldir/$word.syn
+
+done
