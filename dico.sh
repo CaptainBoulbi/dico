0bfd4f8d6fc8b2e14ddcd500c1a869d0116f9f3a
_N3m0 <n3m0o7@hotmail.com>
2023-12-26 14:32:20 +0100

    dico get one def

--- dico.sh
+++ dico.sh
@@ -4,7 +4,7 @@ defaultlang="fr"
 dicodir="/usr/local/dico"
 
 if [[ $1 == "--fr" || $1 == "--en" ]]; then
-  man $dicodir/${1#--}/$2*
+  man $dicodir/${1#--}/$2
 else
-  man $dicodir/$defaultlang/$1*
+  man $dicodir/$defaultlang/$1
 fi
